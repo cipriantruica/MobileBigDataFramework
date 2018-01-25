85ede5701ccb5331bafa96d0639a94f5b81d2c30
cipriantruica <ciprian.octavian.truica@gmail.com>
2018-01-25 19:11:03 +0200

    sql query

--- filtering/ties_query.sql
+++ filtering/ties_query.sql
@@ -1,3 +1,4 @@
+-- entire formula for alpha
 select sid1, sid2, 
 	1 - (select count(distinct sid1) - 2 from edges where MilanoDate=e.MilanoDate)*(
 		EdgeCost/(select sum(EdgeCost) node_strength from edges where sid1 != sid2 and MilanoDate=e.MilanoDate and sid1 = e.sid1 group by sid1) +
@@ -9,6 +10,15 @@ select sid1, sid2,
 	(
 		(select count(distinct sid1) - 1 from edges where MilanoDate=e.MilanoDate) * 
 		(EdgeCost/(select sum(EdgeCost) node_strength from edges where sid1 != sid2 and MilanoDate=e.MilanoDate and sid1 = e.sid1 group by sid1) - 1)
+	)
+from edges e 
+	where MilanoDate='2013-11-01'
+
+-- simplified
+select sid1, sid2, 
+	pow(
+		1 - EdgeCost/(select sum(EdgeCost) node_strength from edges where sid1 != sid2 and MilanoDate=e.MilanoDate and sid1 = e.sid1 group by sid1), 
+		(select count(distinct sid1) - 1 from edges where MilanoDate=e.MilanoDate)
 	)
 from edges e 
 	where MilanoDate='2013-11-01'
