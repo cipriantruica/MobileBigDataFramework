9b406dfd89811be6b63c25103cb085ceafecbcfc
cipriantruica <ciprian.octavian.truica@gmail.com>
2018-02-02 14:53:26 +0100

    remove old louvain modularity

--- louvain-modularity-master/src/main/scala/LouvainConfig.scala
+++
@@ -1,9 +0,0 @@
-case class LouvainConfig(
-  inputFile: String,
-  outputDir: String,
-  hiveSchema: String,
-  hiveInputTable: String,
-  parallelism: Int,
-  minimumCompressionProgress: Int,
-  progressCounter: Int,
-  delimiter: String)
