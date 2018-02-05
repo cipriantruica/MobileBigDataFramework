6de0ca1dee58d8b408d7e85c5e93941b9aa42c44
cipriantruica <ciprian.octavian.truica@gmail.com>
2018-02-05 18:26:17 +0100

    save to hive

--- LouvainModularityHive/src/main/scala/Louvain.scala
+++ LouvainModularityHive/src/main/scala/Louvain.scala
@@ -415,7 +415,7 @@ class Louvain() extends Serializable{
             StructField("edgeCostFactor", IntegerType, true)
           ))
 
-    val df = hc.createDataFrame(vertexRDD).schema(fileSchema)
+    val df = hc.createDataFrame(vertexRDD, fileSchema)
     df.show()
     println("=====================================================")
     // .write.format("orc").mode("append").saveAsTable(config.hiveSchema + "." + config.hiveOutputTable)
