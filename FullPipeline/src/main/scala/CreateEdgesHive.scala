af2c2460923b5eeaa4b3af10278affffa7ddf1e3
cipriantruica <ciprian.octavian.truica@gmail.com>
2018-02-06 14:33:53 +0100

    Fullpipeline for localhost

---
+++ FullPipeline/src/main/scala/CreateEdgesHive.scala
@@ -0,0 +1,53 @@
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.sql.types._
+import org.apache.spark.{SparkConf, SparkContext}
+
+/*
+    It reads all the data from the tcv and computes the edges.
+    It uses a Dataframe with a single select.
+    It saves the output as parquet
+*/
+
+object CreateEdgesHive {
+  def main(args: Array[String]): Unit = {
+    // input directory with the tsv
+    // val inputDirectory = "hdfs://localhost:9000/user/sheepman/input/MI2MI/MItoMI-2013-11*"
+    val inputDirectory = "file:///home/sheepman/DATA_SETS/MItoMI-2013-11*"
+
+    // the tsv schema
+    val fileSchema = StructType(Array(
+      StructField("Timestamp", LongType, true),
+      StructField("SquareID1", IntegerType, true),
+      StructField("SquareID2", IntegerType, true),
+      StructField("DIS", DoubleType, true)))
+
+    val t0 = System.nanoTime()
+
+    // Spark session
+    // Create spark configuration
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Create_Edges_Hive_v1")
+
+    // Create spark context
+    val sc = new SparkContext(sparkConf)
+    // Create Hive context
+    val hc = new HiveContext(sc)
+
+    // read the data from the tsv files
+    val df = hc.read.format("csv")
+      .option("header", "false")
+      .option("delimiter", "\t")
+      .schema(fileSchema)
+      .load(inputDirectory)
+
+    // create a view to query
+    df.createOrReplaceTempView("mi2mi_table")
+
+    // create the edges and save them to parquet files
+    val sqlEdges = hc.sql("select Date MilanoDate, SID1, SID2, sum(DIS) EdgeCost from (select cast(from_unixtime(Timestamp/1000) as Date) Date, SquareID1 SID1, SquareID2 SID2, DIS from mi2mi_table where SquareID1 <= SquareID2 union all select cast(from_unixtime(Timestamp/1000) as Date) Date, SquareID2, SquareID1, DIS from mi2mi_table where SquareID1 > SquareID2) group by Date, SID1, SID2 order by Date, SID1, SID2")
+      .write.format("orc").saveAsTable("edges")
+
+    val t1 = System.nanoTime()
+    println("Elaspsed time (ms): " + ((t1 - t0)/1e6))
+
+  }
+}
---
+++ FullPipeline/src/main/scala/Driver.scala
@@ -0,0 +1,62 @@
+
+import org.apache.spark.graphx.{Edge, Graph}
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.hive.HiveContext
+
+
+object Driver {
+
+  def main(args: Array[String]): Unit ={
+    // the day for wich we compute louvain modularity
+    var date = "2013-11-01" // args(0)
+    // the alpha threshold filter value
+    var alphaThreshold = "0.005"
+    // a constant for changing the edge cost factor
+    var edgeCostFactor = "1000000"
+    val config = LouvainConfig(
+      "mi2mi",
+      "edges",
+      "LinkFiltering",
+      "LouvainCommunity", // this table must be created manualy in Hive
+      date,
+      alphaThreshold,
+      edgeCostFactor,
+      2000,
+      1)
+
+    val t0 = System.nanoTime()
+
+    // Create spark configuration
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Louvain w/ Hive v1 date:" + date)
+
+    // Create spark context
+    val sc = new SparkContext(sparkConf)
+    // Create Hive context
+    val hc = new HiveContext(sc)
+    // create the table if not exists
+    hc.sql("CREATE TABLE IF NOT EXISTS " + config.hiveOutputTable + "(MilanoDate date, SID1 int, community int, level int, alphaThreshold int, edgeCostFactor int)")
+
+    // verify if the louvain modularity was already computed
+    val louvainTbl = hc.table(config.hiveOutputTable)
+    // register the table so it can be used in SQL
+    louvainTbl.createOrReplaceTempView(config.hiveOutputTable)
+
+    val exists = hc.sql("select count(MilanoDate) from " + config.hiveOutputTable + " where MilanoDate = '" + config.dateInput + "' and edgeCostFactor = " + config.edgeCostFactor + " and alphaThreshold = " + config.alphaThreshold + " * 1000" )
+    // val exists = hc.sql("select count(MilanoDate), alphaThreshold from " + config.hiveOutputTable + " where MilanoDate = '" + config.dateInput + "' and edgeCostFactor = " + config.edgeCostFactor + " group by alphaThreshold")
+    exists.show()
+    println("exists: " + exists.first().getLong(0))
+    if(exists.first().getLong(0) == 0){
+      val louvain = new Louvain()
+      louvain.run(sc, hc, config)  
+    }
+    else{
+      println("Louvain Modularity was already computer for MilanoDate = " + config.dateInput + " and alphaThreshold = " + config.alphaThreshold + " and edgeCostFactor =" + config.edgeCostFactor)
+    }
+
+    val t1 = System.nanoTime()
+    println("Elaspsed time (ms): " + ((t1 - t0)/1e6))
+    
+
+  }
+}
