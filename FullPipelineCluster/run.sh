ee73dda715d51ca7f9aa0bff0397b2596f8ef826
cipriantruica <ciprian.octavian.truica@gmail.com>
2018-02-06 17:57:50 +0100

    added the automated script for testing

--- FullPipelineCluster/run.sh
+++ FullPipelineCluster/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-for i in `seq 2 10`
+for i in `seq 1 10`
 do
 	spark-submit --master yarn --deploy-mode client --num-executors 16 --executor-cores 1 --executor-memory 3G --class CreateEdgesHive target/scala-2.11/fullpipelinecluster_2.11-0.1.jar $i >> "results/output_CreateEdgesHive"
 
