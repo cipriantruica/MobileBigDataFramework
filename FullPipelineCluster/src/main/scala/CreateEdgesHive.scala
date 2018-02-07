57500b3dfdd042f38d208adafb3e6ddbea407b3d
cipriantruica <ciprian.octavian.truica@gmail.com>
2018-02-07 15:54:24 +0100

    rearranged he code

--- FullPipelineCluster/src/main/scala/CreateEdgesHive.scala
+++ FullPipelineCluster/src/main/scala/CreateEdgesHive.scala
@@ -15,63 +15,62 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object CreateEdgesHive {
   def main(args: Array[String]): Unit = {
-        // input directory with the tsv
-        val inputDirectory = "hdfs://hadoop-master:8020/user/ciprian/input/MI2MI/MItoMI-2013-11*" 
-        // val inputDirectory = args(0)
+    // input directory with the tsv
+    val inputDirectory = "hdfs://hadoop-master:8020/user/ciprian/input/MI2MI/MItoMI-2013-11*" 
+    // val inputDirectory = args(0)
 
-        // the file with the mearsuments
-        val printFile = "./results/runtime_Create_Edges_Hive_test_" + args(0) + ".txt"
-        // val printFile = args(2)
+    // the file with the mearsuments
+    val printFile = "./results/runtime_Create_Edges_Hive_test_" + args(0) + ".txt"
+    // val printFile = args(2)
 
-        // the tsv schema
-        val fileSchema = StructType(Array(
-            StructField("Timestamp", LongType, true),
-            StructField("SquareID1", IntegerType, true),
-            StructField("SquareID2", IntegerType, true),
-            StructField("DIS", DoubleType, true)))
+    // the tsv schema
+    val fileSchema = StructType(Array(
+        StructField("Timestamp", LongType, true),
+        StructField("SquareID1", IntegerType, true),
+        StructField("SquareID2", IntegerType, true),
+        StructField("DIS", DoubleType, true)))
 
+    // Spark session
+    // Create spark configuration
+    val sparkConf = new SparkConf().setAppName("Create Edges Hive Test no. " + args(0))
 
-        // Spark session
-        // Create spark configuration
-        val sparkConf = new SparkConf().setAppName("Create Edges Hive Test no. " + args(0))
+    // Create spark context
+    val sc = new SparkContext(sparkConf)
+    // Create Hive context
+    val hc = new HiveContext(sc)
+    // drop table if it exists
+    hc.sql("drop table if exists mi2mi.edges")
 
-        // Create spark context
-        val sc = new SparkContext(sparkConf)
-        // Create Hive context
-        val hc = new HiveContext(sc)
-        // drop table if it exists
-        hc.sql("drop table if exists mi2mi.edges")
+    // PrintWriter
+    import java.io._
+    val pw = new PrintWriter(new File(printFile))
 
-        // PrintWriter
-        import java.io._
-        val pw = new PrintWriter(new File(printFile))
+    pw.println("Create Edges Hive")
+    pw.println("Start time: " + Calendar.getInstance().getTime())
 
-        pw.println("Create Edges Hive")
-        pw.println("Start time: " + Calendar.getInstance().getTime())
+    val t0 = System.nanoTime()
 
-        val t0 = System.nanoTime()
+    // read the data from the tsv files
+    val df = hc.read.format("csv")
+        .option("header", "false")
+        .option("delimiter", "\t")
+        .schema(fileSchema)
+        .load(inputDirectory)
 
-        // read the data from the tsv files
-        val df = hc.read.format("csv")
-            .option("header", "false")
-            .option("delimiter", "\t")
-            .schema(fileSchema)
-            .load(inputDirectory)
+    // create a view to query
+    df.createOrReplaceTempView("mi2mi_table")
 
-        // create a view to query
-        df.createOrReplaceTempView("mi2mi_table")
+    // create the edges and save them to parquet files
+    val sqlEdges = hc.sql("select Date MilanoDate, SID1, SID2, sum(DIS) EdgeCost from (select cast(from_unixtime(Timestamp/1000) as Date) Date, SquareID1 SID1, SquareID2 SID2, DIS from mi2mi_table where SquareID1 <= SquareID2 union all select cast(from_unixtime(Timestamp/1000) as Date) Date, SquareID2, SquareID1, DIS from mi2mi_table where SquareID1 > SquareID2) group by Date, SID1, SID2")
+        .write.format("orc").saveAsTable("mi2mi.edges")
 
-        // create the edges and save them to parquet files
-        val sqlEdges = hc.sql("select Date MilanoDate, SID1, SID2, sum(DIS) EdgeCost from (select cast(from_unixtime(Timestamp/1000) as Date) Date, SquareID1 SID1, SquareID2 SID2, DIS from mi2mi_table where SquareID1 <= SquareID2 union all select cast(from_unixtime(Timestamp/1000) as Date) Date, SquareID2, SquareID1, DIS from mi2mi_table where SquareID1 > SquareID2) group by Date, SID1, SID2")
-            .write.format("orc").saveAsTable("mi2mi.edges")
+    val t1 = System.nanoTime()
+    pw.println("End time: " + Calendar.getInstance().getTime())
+    pw.println("Elapsed time (ms): " + ((t1 - t0)/1e6))
+    println("Create Edges Hive Test no. " + args(0))
+    println("Elapsed time (ms): " + ((t1 - t0)/1e6))
+    pw.println("*************************************************")
 
-        val t1 = System.nanoTime()
-        pw.println("End time: " + Calendar.getInstance().getTime())
-        pw.println("Elapsed time (ms): " + ((t1 - t0)/1e6))
-        println("Create Edges Hive Test no. " + args(0))
-        println("Elapsed time (ms): " + ((t1 - t0)/1e6))
-        pw.println("*************************************************")
-
-        pw.close()
-    }
+    pw.close()
+  }
 }
--- FullPipelineCluster/src/main/scala/LinkFilteringHive.scala
+++ FullPipelineCluster/src/main/scala/LinkFilteringHive.scala
@@ -15,56 +15,56 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object LinkFilteringHive {
   def main(args: Array[String]): Unit = {
-        // the file with the mearsuments
-        val printFile = "./results/runtime_Link_Filtering_Hive_test_" + args(0) + ".txt"
+    // the file with the mearsuments
+    val printFile = "./results/runtime_Link_Filtering_Hive_test_" + args(0) + ".txt"
 
-        // Spark session
-        // Create spark configuration
-        val sparkConf = new SparkConf().setAppName("Link Filtering Hive Test no. " + args(0))
+    // Spark session
+    // Create spark configuration
+    val sparkConf = new SparkConf().setAppName("Link Filtering Hive Test no. " + args(0))
 
-        // Create spark context
-        val sc = new SparkContext(sparkConf)
-        // Create Hive context
-        val hc = new HiveContext(sc)
+    // Create spark context
+    val sc = new SparkContext(sparkConf)
+    // Create Hive context
+    val hc = new HiveContext(sc)
 
-        hc.sql("drop table if exists mi2mi.LinkFiltering")
+    hc.sql("drop table if exists mi2mi.LinkFiltering")
 
-        import java.io._
-        val pw = new PrintWriter(new File(printFile))
+    import java.io._
+    val pw = new PrintWriter(new File(printFile))
 
-        pw.println("Link Filtering Hive")
-        pw.println("Start time: " + Calendar.getInstance().getTime())
+    pw.println("Link Filtering Hive")
+    pw.println("Start time: " + Calendar.getInstance().getTime())
 
-        val t0 = System.nanoTime()
-        // read the data from the Hive (mi2mi - is the database name, edges is the table name)
-        val tbl = hc.table("mi2mi.edges")
-        // register the table so it can be used in SQL
-        tbl.createOrReplaceTempView("edges")
+    val t0 = System.nanoTime()
+    // read the data from the Hive (mi2mi - is the database name, edges is the table name)
+    val tbl = hc.table("mi2mi.edges")
+    // register the table so it can be used in SQL
+    tbl.createOrReplaceTempView("edges")
 
-        // create the alpha from equation (1) for each day 
-        // equation (1) has the following solution after computations : alpha_ij = (1-p_ij)^(k - 1) where: 
-        // k is the number of nodes
-        // p_ij = w_ij/(sum_t (w_it) ) 
-        // where: 
-        //   w_ij is the Edge cost between node i and j
-        //   sum_t is the sum of all the edge cost for node i
-        // the following SQL query is used compute alpha for the entire data set and the results are stored in Hive
-        // (currently in Hive are stored only the data for November)
-        // see the ties_query.sql for the query explanations
-        val ties = hc.sql("select MilanoDate, sid1, sid2, pow(1 - EdgeCost/(select sum(EdgeCost) node_strength from edges where sid1 != sid2 and MilanoDate=e.MilanoDate and sid1 = e.sid1 group by sid1), (select count(distinct sid1) - 1 from edges where MilanoDate=e.MilanoDate)) alpha from edges e where sid1 != sid2")
-        ties.write.format("orc").saveAsTable("mi2mi.LinkFiltering")
+    // create the alpha from equation (1) for each day 
+    // equation (1) has the following solution after computations : alpha_ij = (1-p_ij)^(k - 1) where: 
+    // k is the number of nodes
+    // p_ij = w_ij/(sum_t (w_it) ) 
+    // where: 
+    //   w_ij is the Edge cost between node i and j
+    //   sum_t is the sum of all the edge cost for node i
+    // the following SQL query is used compute alpha for the entire data set and the results are stored in Hive
+    // (currently in Hive are stored only the data for November)
+    // see the ties_query.sql for the query explanations
+    val ties = hc.sql("select MilanoDate, sid1, sid2, pow(1 - EdgeCost/(select sum(EdgeCost) node_strength from edges where sid1 != sid2 and MilanoDate=e.MilanoDate and sid1 = e.sid1 group by sid1), (select count(distinct sid1) - 1 from edges where MilanoDate=e.MilanoDate)) alpha from edges e where sid1 != sid2")
+    ties.write.format("orc").saveAsTable("mi2mi.LinkFiltering")
 
-        // tried a filter for alpha_threshold == 0.5
-        // ties.filter(ties("alpha") <= 0.05).show()
+    // tried a filter for alpha_threshold == 0.5
+    // ties.filter(ties("alpha") <= 0.05).show()
 
-        val t1 = System.nanoTime()
+    val t1 = System.nanoTime()
 
-        pw.println("End time: " + Calendar.getInstance().getTime())
-        pw.println("Elapsed time (ms): " + ((t1 - t0)/1e6))
-	println("Link Filtering Hive Test no. " + args(0))
-	println("Elapsed time (ms): " + ((t1 - t0)/1e6))
-        pw.println("*************************************************")
+    pw.println("End time: " + Calendar.getInstance().getTime())
+    pw.println("Elapsed time (ms): " + ((t1 - t0)/1e6))
+	  println("Link Filtering Hive Test no. " + args(0))
+	  println("Elapsed time (ms): " + ((t1 - t0)/1e6))
+    pw.println("*************************************************")
 
-        pw.close()
-    }
+    pw.close()
+  }
 }
