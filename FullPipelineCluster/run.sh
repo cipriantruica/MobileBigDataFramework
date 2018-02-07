6d29f08eab3f8a502137e5e3a742ae97d75ec601
cipriantruica <ciprian.octavian.truica@gmail.com>
2018-02-07 10:53:30 +0100

    run script

--- FullPipelineCluster/run.sh
+++ FullPipelineCluster/run.sh
@@ -5,5 +5,5 @@ for i in `seq 1 1`
 do
 	# spark-submit --master yarn --deploy-mode client --num-executors 16 --executor-cores 1 --executor-memory 3G --class CreateEdgesHive target/scala-2.11/fullpipelinecluster_2.11-0.1.jar $i >> "results/output_CreateEdgesHive"
 	# spark-submit --master yarn --deploy-mode client --num-executors 16 --executor-cores 1 --executor-memory 3G --class LinkFilteringHive target/scala-2.11/fullpipelinecluster_2.11-0.1.jar $i >> "results/output_LinkFilteringHive"
-	spark-submit --master yarn --deploy-mode client --num-executors 16 --executor-cores 1 --executor-memory 3G --class CreateEdgesAlphaHive target/scala-2.11/fullpipelinecluster_2.11-0.1.jar $i >> "results/output_CreateEdgesHive"
+	spark-submit --master yarn --deploy-mode client --num-executors 16 --executor-cores 1 --executor-memory 3G --class CreateEdgesAlphaHive target/scala-2.11/fullpipelinecluster_2.11-0.1.jar $i >> "results/output_CreateEdgesAlphaHive"
 done;
