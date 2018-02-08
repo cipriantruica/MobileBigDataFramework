66477a3e95e493186d7705df9c319a581f075c6a
cipriantruica <ciprian.octavian.truica@gmail.com>
2018-02-08 17:29:22 +0100

    run lmh

--- FullPipelineCluster/run_lmh.sh
+++ FullPipelineCluster/run_lmh.sh
@@ -12,10 +12,10 @@ do
 
     for i in `seq 1 10`
     do
-      spark-submit --master yarn --deploy-mode client --num-executors 16 --executor-cores 1 --executor-memory 3G --class Driver target/scala-2.11/fullpipelinecluster_2.11-0.1.jar $date 0.05 1000000 1 $i >> "results/output_lmh_1_"$date
+      spark-submit --master yarn --deploy-mode client --num-executors 16 --executor-cores 1 --executor-memory 3G --class Driver target/scala-2.11/fullpipelinecluster_2.11-0.1.jar $date 0.05 1000000 1 $i >> "results/output_lmh_1TBL_"$date
       sleep 10
 
-      spark-submit --master yarn --deploy-mode client --num-executors 16 --executor-cores 1 --executor-memory 3G --class Driver target/scala-2.11/fullpipelinecluster_2.11-0.1.jar $date 0.05 1000000 2 $i >> "results/output_lmh_2_"$date
+      spark-submit --master yarn --deploy-mode client --num-executors 16 --executor-cores 1 --executor-memory 3G --class Driver target/scala-2.11/fullpipelinecluster_2.11-0.1.jar $date 0.05 1000000 2 $i >> "results/output_lmh_2TBL_"$date
       sleep 10
 
   done;
