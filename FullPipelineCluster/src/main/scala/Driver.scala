e5dea0fb785282c2f8533eca7bde3370769f7e24
cipriantruica <ciprian.octavian.truica@gmail.com>
2018-02-08 18:41:56 +0100

    tables

--- FullPipelineCluster/src/main/scala/Driver.scala
+++ FullPipelineCluster/src/main/scala/Driver.scala
@@ -33,7 +33,7 @@ object Driver {
     // the file with the mearsuments
     val printFile = "./results/runtime_LMH_" + date + "_noTbls_" + noTables + "_test_" + args(4) + ".txt"
     // Create spark configuration
-    val sparkConf = new SparkConf().setAppName("Louvain with Hive Test no. " + args(0) + " for date: " + config.dateInput + " and alphaThreshold = " + config.alphaThreshold + " and edgeCostFactor =" + config.edgeCostFactor)
+    val sparkConf = new SparkConf().setAppName("Louvain with Hive Test no. " + args(4) + " for date: " + config.dateInput + " and alphaThreshold = " + config.alphaThreshold + " and edgeCostFactor =" + config.edgeCostFactor)
 
     // Create spark context
     val sc = new SparkContext(sparkConf)
@@ -71,7 +71,7 @@ object Driver {
     val t1 = System.nanoTime()
     pw.println("End time: " + Calendar.getInstance().getTime())
     pw.println("Elapsed time (ms): " + ((t1 - t0)/1e6))
-    println("Louvain with Hive Test no. " + args(0) + " for date: " + config.dateInput + " and alphaThreshold = " + config.alphaThreshold + " and edgeCostFactor =" + config.edgeCostFactor)
+    println("Louvain with Hive Test no. " + args(4) + " for date: " + config.dateInput + " and alphaThreshold = " + config.alphaThreshold + " and edgeCostFactor =" + config.edgeCostFactor)
     println("Elapsed time (ms): " + ((t1 - t0)/1e6))
     pw.println("*************************************************")
 
