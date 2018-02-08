72b78fd91174a262096dbdc641549bceab0fecb2
cipriantruica <ciprian.octavian.truica@gmail.com>
2018-02-08 15:12:31 +0100

    run.sh new tests

--- FullPipelineCluster/run.sh
+++ FullPipelineCluster/run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 
-for i in `seq 1 10`
+for i in `seq 11 20`
 do
 	# spark-submit --master yarn --deploy-mode client --num-executors 16 --executor-cores 1 --executor-memory 3G --class CreateEdgesHive target/scala-2.11/fullpipelinecluster_2.11-0.1.jar $i >> "results/output_CreateEdgesHive"
-	# spark-submit --master yarn --deploy-mode client --num-executors 16 --executor-cores 1 --executor-memory 3G --class LinkFilteringHive target/scala-2.11/fullpipelinecluster_2.11-0.1.jar $i >> "results/output_LinkFilteringHive"
-	spark-submit --master yarn --deploy-mode client --num-executors 16 --executor-cores 1 --executor-memory 3G --class CreateEdgesAlphaHive target/scala-2.11/fullpipelinecluster_2.11-0.1.jar $i >> "results/output_CreateEdgesAlphaHive"
+	# sleep 10
+	
+	spark-submit --master yarn --deploy-mode client --num-executors 16 --executor-cores 1 --executor-memory 3G --class LinkFilteringHive target/scala-2.11/fullpipelinecluster_2.11-0.1.jar $i >> "results/output_LinkFilteringHive"
+	sleep 10
+
+	# spark-submit --master yarn --deploy-mode client --num-executors 16 --executor-cores 1 --executor-memory 3G --class CreateEdgesAlphaHive target/scala-2.11/fullpipelinecluster_2.11-0.1.jar $i >> "results/output_CreateEdgesAlphaHive"
+	# sleep 10
 done;
