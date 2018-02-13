ec9d570abe883d75733a06b185d615ee21f7fbef
cipriantruica <ciprian.octavian.truica@gmail.com>
2018-02-13 10:34:02 +0100

    run script Jaccard Coefficient

--- FullPipelineCluster/run_jc.sh
+++ FullPipelineCluster/run_jc.sh
@@ -9,11 +9,13 @@ NUM_CORES=1
 MEM_EXECS=3G
 NUM_TESTS=1
 JAR_FILE=target/scala-2.11/fullpipelinecluster_2.11-0.1.jar
-INPUT_DATA=hdfs://hadoop-master:8020/user/ciprian/input/MI2MI/MItoMI-2013-11*
+DRIVER_MEM=4G
+DRIVER_CORES=4
+
 
 for i in `seq 1 $NUM_TESTS`
 do
     # build Jaccard Coefficient table in Hive
-	spark-submit --master $MASTER --deploy-mode $MODE --num-executors $NUM_EXECS --executor-cores $NUM_CORES --executor-memory $MEM_EXECS --class JaccardCoefficient $JAR_FILE $i >> "results/output_JaccardCoefficientHive"
+	spark-submit --master $MASTER --deploy-mode $MODE --driver-memory $DRIVER_MEM --driver-cores $DRIVER_CORES --num-executors $NUM_EXECS --executor-cores $NUM_CORES --executor-memory $MEM_EXECS --class JaccardCoefficient $JAR_FILE $i >> "results/output_JaccardCoefficientHive"
 	sleep 10
 done;
