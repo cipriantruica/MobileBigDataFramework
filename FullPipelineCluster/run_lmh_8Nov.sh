1165dfa7b065335c357541d9166fd838a85b9b58
cipriantruica <ciprian.octavian.truica@gmail.com>
2018-02-27 06:42:24 +0200

    run script

--- FullPipelineCluster/run_lmh_8Nov.sh
+++ FullPipelineCluster/run_lmh_8Nov.sh
@@ -17,7 +17,7 @@ DATE='2013-11-08'
 ECF=1000000000000 #EDGE COST FACTOR - used for normalizing the weight of a edge - this is the maximum needed for date 2013-11-08
 
 #alpha=1 - no filtering
-spark-submit --master $MASTER --deploy-mode $MODE --num-executors $NUM_EXECS --executor-cores $NUM_CORES --executor-memory $MEM_EXECS --class Driver $JAR_FILE $DATE $ALPHA1 $ECF $NO_TBL 1 >> "results_lhm/output_LMH_"$NO_TBL"TBL_"$DATE"_alpha_"$ALPHA1"_ECF_"$ECF
+spark-submit --master $MASTER --deploy-mode $MODE --num-executors $NUM_EXECS --executor-cores $NUM_CORES --executor-memory $MEM_EXECS --class Driver $JAR_FILE $DATE $ALPHA0 $ECF $NO_TBL 1 >> "results_lhm/output_LMH_"$NO_TBL"TBL_"$DATE"_alpha_"$ALPHA0"_ECF_"$ECF
 sleep 10
 
 #alpha=0.05
