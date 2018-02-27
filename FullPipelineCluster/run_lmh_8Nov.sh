482720a1a19837c8406a9b17ebaf4897c54679e2
cipriantruica <ciprian.octavian.truica@gmail.com>
2018-02-27 12:58:56 +0200

    run script create csv

--- FullPipelineCluster/run_lmh_8Nov.sh
+++ FullPipelineCluster/run_lmh_8Nov.sh
@@ -16,7 +16,7 @@ NO_TBL=2
 DATE='2013-11-08'
 ECF=1000000000000 #EDGE COST FACTOR - used for normalizing the weight of a edge - this is the maximum needed for date 2013-11-08
 
-alpha=0.001
+#alpha=0.001
 spark-submit --master $MASTER --deploy-mode $MODE --num-executors $NUM_EXECS --executor-cores $NUM_CORES --executor-memory $MEM_EXECS --class Driver $JAR_FILE $DATE $ALPHA3 $ECF $NO_TBL 1 >> "results_lhm/output_LMH_"$NO_TBL"TBL_"$DATE"_alpha_"$ALPHA3"_ECF_"$ECF
 sleep 10
 
