e9cbb68f517d8557d87bc95bae60b31a7df9c916
cipriantruica <ciprian.octavian.truica@gmail.com>
2018-02-27 12:32:49 +0200

    added correct comments & license

--- FullPipelineCluster/src/main/scala/LinkFilteringHive.scala
+++ FullPipelineCluster/src/main/scala/LinkFilteringHive.scala
@@ -4,9 +4,27 @@ import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.{SparkConf, SparkContext}
 
 /*
-    It reads all the data from the tcv and computes the edges.
+ * Copyright (C) 2018 Ciprian-Octavian Truică <ciprian.truica@cs.pub.ro>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+    It reads all the data from Hive.
     It uses a Dataframe with a single select.
-    It saves the output as parquet
+    Computes alphas for the entire dataset.
+    Saves the output in Hive
 */
 
 object LinkFilteringHive {
